b5aa2850cfb2c2d562c6458f006a33c361da18de
Andrej Kvasnica <andrej@gmail.com>
2012-10-23 17:42:28 +0200

    adding office stuff

--- f17-all.sh
+++ f17-all.sh
@@ -3,3 +3,5 @@
 ./f17-core.sh
 ./f17-internet.sh
 ./f17-media.sh
+./f17-office.sh
+
---
+++ f17-office.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# office stuff
+# download size: 96 M
+# installed size: 295 M
+
+yum install libreoffice-calc libreoffice-writer libreoffice-impress libreoffice-presentation-minimizer antiword
+
